2c7537014a444f3e18a46aad52c42b3dfd504eae
Rahul <rahulscodingworld@gmail.com>
2022-05-18 18:18:56 -0400

    fixed styling on modal

--- components/Modal.js
+++ components/Modal.js
@@ -6,7 +6,7 @@ const Modal = ( {setShowModal}) => {
 
   return (
 <div className="bg-off fonds backdrop-blur-md ">
-    <div  className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex">
+    <div  className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 md:h-full grid justify-items-center items-center h-screen">
     <div className="relative p-4 w-full max-w-md h-full md:h-auto">
 
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -18,14 +18,19 @@ const Modal = ( {setShowModal}) => {
                 <form className="space-y-6" action="#">
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="John Doe" required/>
+                        <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg drop-shadow-lg  block w-full p-2.5 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="John Doe" required/>
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Message</label>
-                        <textarea type="password" name="password" id="password" placeholder="I need more cookies!!!" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required> </textarea>
+                
+                        <textarea
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 drop-shadow-lg dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="Enter your message here..."
+                  required
+              ></textarea>
                     </div>
                     
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send this Message</button>
+                    <button type="submit" className="w-full btn ">Send!</button>
                     
                 </form>
             </div>
